8768e103c6d37847a6fcbfc7831d59dff54bd1b5
oozaki <christopher@sapsed.co.uk>
2020-08-01 18:53:09 +0100

    Added Movement

---
+++ Game/Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterMotor : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    public void setDestination(Vector3 pos)
+    {
+        agent.destination = pos;
+    }
+
+    void FixedUpdate(){
+        var horizontalVelocity = new Vector3(agent.velocity.x, 0, agent.velocity.z);
+        var isMoving = horizontalVelocity.magnitude > 0;
+        Debug.Log(agent.velocity);
+        if(isMoving){
+
+        
+            if((transform.position - agent.destination).magnitude > 5){
+                animator.SetBool("Running", true);
+                animator.SetBool("Moving", false);
+                agent.speed = 5;
+            }
+            else {
+                animator.SetBool("Moving",true);
+                animator.SetBool("Running", false);
+                agent.speed = 3;
+            }   
+        }
+        else{
+            animator.SetBool("Moving", false);
+            animator.SetBool("Running", false);
+        }
+        
+    }
+}
---
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(CharacterMotor))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 mousePosition; 
+    private CharacterMotor characterMotor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterMotor = GetComponent<CharacterMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMove(InputAction.CallbackContext context){
+        //mousePosition = Mouse.current.position;
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hitData;
+
+        if(Physics.Raycast(ray, out hitData, 1000)) {
+            var updatedPos = new Vector3(Mathf.Round(hitData.point.x/5)*5, hitData.point.y, Mathf.Round(hitData.point.z/5)*5);
+            characterMotor.setDestination(updatedPos);
+            
+        }
+    }
+}
