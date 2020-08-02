61ff63ff13da9e26f360eeaaa921d8cd85bc14c1
oozaki <christopher@sapsed.co.uk>
2020-08-02 19:58:05 +0100

    WIP

--- Game/Assets/Scripts/CharacterMotor.cs
+++ Game/Assets/Scripts/CharacterMotor.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 public class CharacterMotor : MonoBehaviour
 {
     NavMeshAgent agent;
     Animator animator;
 
+    public bool isMoving;
+    private bool isAttacking;
+
+    public GameObject playerMarker;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponentInChildren<Animator>();
+        playerMarker.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,26 +30,38 @@ public class CharacterMotor : MonoBehaviour
 
     void FixedUpdate(){
         var horizontalVelocity = new Vector3(agent.velocity.x, 0, agent.velocity.z);
-        var isMoving = horizontalVelocity.magnitude > 0;
+        isMoving = horizontalVelocity.magnitude > 0.5;
         Debug.Log(agent.velocity);
-        if(isMoving){
-
         
-            if((transform.position - agent.destination).magnitude > 6){
-                animator.SetBool("Running", true);
-                animator.SetBool("Moving", false);
-                agent.speed = 5;
+        
+            if(isMoving){
+                playerMarker.transform.position = agent.destination;
+                playerMarker.SetActive(true);
+            
+                if((transform.position - agent.destination).magnitude > 6){
+                    animator.SetBool("Running", true);
+                    animator.SetBool("Moving", false);
+                    agent.speed = 5;
+                }
+                else {
+                    animator.SetBool("Moving",true);
+                    animator.SetBool("Running", false);
+                    agent.speed = 3;
+                }   
             }
-            else {
-                animator.SetBool("Moving",true);
-                animator.SetBool("Running", false);
-                agent.speed = 3;
-            }   
-        }
-        else{
-            animator.SetBool("Moving", false);
-            animator.SetBool("Running", false);
-        }
+            else{
+                    animator.SetBool("Moving", false);
+                    animator.SetBool("Running", false);
+                    
+                playerMarker.SetActive(false);
+            }
         
     }
+
+    public void OnAttack(){
+        animator.SetTrigger("Attack");
+        isAttacking = true;
+    }
+
+    
 }
--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -9,28 +9,41 @@ using UnityEngine.AI;
 public class PlayerMovement : MonoBehaviour
 {
     private Vector2 mousePosition; 
+
+    
     private CharacterMotor characterMotor;
+    private Vector3 updatedPos;
+    private bool isValid;
+
+    public GameObject playerMarker;
     // Start is called before the first frame update
     void Start()
     {
         characterMotor = GetComponent<CharacterMotor>();
+        playerMarker.SetActive(false);
     }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hitData;
+        if(!characterMotor.isMoving && Physics.Raycast(ray, out hitData, 1000)) {
+            updatedPos = new Vector3(Mathf.Round(hitData.point.x/5)*5, hitData.point.y, Mathf.Round(hitData.point.z/5)*5);
+            playerMarker.transform.position = updatedPos;
+            playerMarker.SetActive(true);
+            isValid = true;
+        }
+        else{
+            playerMarker.SetActive(false);
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context){
-        //mousePosition = Mouse.current.position;
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-        RaycastHit hitData;
-
-        if(Physics.Raycast(ray, out hitData, 1000)) {
-            var updatedPos = new Vector3(Mathf.Round(hitData.point.x/5)*5, hitData.point.y, Mathf.Round(hitData.point.z/5)*5);
-            characterMotor.setDestination(updatedPos);
-            
+        if(!characterMotor.isMoving){
+            //mousePosition = Mouse.current.position;
+            if(isValid){
+                characterMotor.setDestination(updatedPos);
+            }
         }
     }
 }
