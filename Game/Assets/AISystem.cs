f8dd52c257d176cd006e4804cfe116f87ce74b2c
Jeremy Sapsed <jms737800@gmail.com>
2020-08-04 20:51:20 +0100

    Added action points and turn system

---
+++ Game/Assets/AISystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISystem : MonoBehaviour
+{
+    public event Action FinishedTurn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartTurn()
+    {
+        Debug.Log("Processing AI");
+        StartCoroutine(WaitAndFinish());
+    }
+
+    IEnumerator WaitAndFinish()
+    {
+        yield return new WaitForSeconds(3);
+        FinishedTurn?.Invoke();
+    }
+}
---
+++ Game/Assets/TurnSystemUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnSystemUI : MonoBehaviour
+{
+    public TMP_Text text;
+
+    private TurnSystem turnSystem;
+    private Button endTurn;
+
+    void Awake()
+    {
+        endTurn = GetComponentInChildren<Button>();
+        turnSystem = FindObjectOfType<TurnSystem>();
+        turnSystem.PlayerTurnStarted += UpdateUI;
+        turnSystem.PlayerTurnStarted += EnableUI;
+        turnSystem.PlayerTurnEnded += DisableUI;
+    }
+    
+    private void UpdateUI(int turn)
+    {
+        text.text = $"Turn {turn}";
+    }
+
+    private void EnableUI(int _)
+    {
+        endTurn.interactable = true;
+    }
+
+    private void DisableUI()
+    {
+        endTurn.interactable = false;
+    }
+
+    public void EndTurn()
+    {
+        turnSystem.EndTurn();
+    }
+}
