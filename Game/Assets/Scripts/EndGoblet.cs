fc8b0d304b5b923a5b9f0e91d5813d3a7c1cc82e
Jeremy Sapsed <jms737800@gmail.com>
2020-08-08 09:49:01 +0100

    Tweaks and added end goal

---
+++ Game/Assets/Scripts/EndGoblet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CoreSystems.TransitionSystem;
+using UnityEngine;
+
+public class EndGoblet : Interactable
+{
+    public override void Interact(GameObject intiatingObject)
+    {
+        LevelLoader.Instance.LoadLevel(Level.Menu);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
